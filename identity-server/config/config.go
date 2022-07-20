c342b63912acbd331e3e58b3134358277e043935
Dmitry Gavrilov <dmitry.gavrilov@unity3d.com>
2022-07-20 00:35:49 -0400

    add configuration reading

--- identity-server/config/config.go
+++ identity-server/config/config.go
@@ -1,5 +1,16 @@
 package config
 
+import (
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	DefaultLogLevel      = 3     // INFO log level
+	DefaultJWTExpiration = 86400 // 1 day in seconds
+)
+
 type Config struct {
 	Server
 	Postgres
@@ -8,6 +19,7 @@ type Config struct {
 }
 
 type JWT struct {
+	Exp int `mapstructure:"jwt_expiration"`
 }
 
 type Postgres struct {
@@ -17,4 +29,36 @@ type Server struct {
 }
 
 type Logging struct {
+	LogLevel int `mapstructure:"log_level"`
+}
+
+func LoadConfigWithPath(path string) (*Config, error) {
+	if _, err := os.Stat(path); err == nil {
+		viper.SetConfigFile(path)
+	}
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	err = viper.Unmarshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("ini")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config/")
+	viper.AddConfigPath("./etc/")
+	viper.AddConfigPath("/usr/dapper-labs/etc/")
+
+	viper.SetDefault("log_level", DefaultLogLevel)
+	viper.SetDefault("jwt_expiration", DefaultJWTExpiration)
 }
